d047c8f06f17595942f3a86fc6e2971be5644640
Lewis Anderson <467642@chrome.sussexdowns.ac.uk>
2018-11-28 12:19:58 +0000

    my second save(made clicking system)

---
+++ Assets/Scripts/MainButtonClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainButtonClick : MonoBehaviour {
+
+    public GameObject TextBox;
+
+    public void ClickButton() {
+        GlobalCoins.CoinCount += 1;
+    }
+    
+
+}
