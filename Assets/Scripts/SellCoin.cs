89ab97f7e60e0e37a383665254bcf368d15f8977
Lewis Anderson <467642@chrome.sussexdowns.ac.uk>
2018-11-28 14:34:46 +0000

    made sell button

---
+++ Assets/Scripts/SellCoin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellCoin : MonoBehaviour {
+
+    public GameObject TextBox;
+
+    public void ClickButton() {
+        GlobalCoins.CoinCount -= 1;
+        GlobalCash.CashCount += 1;
+    }
+    
+
+}
