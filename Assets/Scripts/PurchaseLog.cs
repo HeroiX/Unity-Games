e65c35b793d914256629d899c05493a1f3e86599
Lewis Anderson <467642@chrome.sussexdowns.ac.uk>
2018-11-29 14:37:56 +0000

    3rd save(auto miner)

---
+++ Assets/Scripts/PurchaseLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseLog : MonoBehaviour {
+
+    public GameObject AutoCoin;
+
+    public void StartAutoCoin()
+    {
+        AutoCoin.SetActive(true);
+    }
+
+
+
+}
--- Assets/Scripts/SellCoin.cs
+++ Assets/Scripts/SellCoin.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SellCoin : MonoBehaviour {
 
-    public GameObject TextBox;
+    public GameObject textBox;
+    public GameObject StatusBox;
 
     public void ClickButton() {
-        GlobalCoins.CoinCount -= 1;
-        GlobalCash.CashCount += 1;
+
+        if (GlobalCoins.CoinCount == 0)
+        {
+            StatusBox.GetComponent<Text>().text = "Not Enough Coins To Sell.";
+            StatusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else
+        {
+            GlobalCoins.CoinCount -= 1;
+            GlobalCash.CashCount += 1;
+        }
     }
+
     
 
 }
